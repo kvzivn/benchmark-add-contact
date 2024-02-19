4f639ab8fa4740120b93793ddede2ec30e352f37
Kevin <hello@kevinivan.com>
2024-02-19 14:47:49 +0800

    better error handling

--- netlify/functions/benchmark-add-contact.js
+++ netlify/functions/benchmark-add-contact.js
@@ -5,30 +5,54 @@ exports.handler = async (event) => {
     return { statusCode: 405, body: "Method Not Allowed" }
   }
 
-  const { email } = JSON.parse(event.body)
+  let body
+  try {
+    body = JSON.parse(event.body)
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: "Cannot parse body: " + error.message,
+    }
+  }
+
+  const { email } = body
   const API_ENDPOINT = `https://clientapi.benchmarkemail.com/Contact/21019657/ContactDetails`
   const AUTH_TOKEN = process.env.BENCHMARK_API_KEY
 
-  return fetch(API_ENDPOINT, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      AuthToken: AUTH_TOKEN,
-    },
-    body: JSON.stringify({
-      Data: {
-        Email: email,
-        EmailPerm: "1",
+  try {
+    const response = await fetch(API_ENDPOINT, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        AuthToken: AUTH_TOKEN,
       },
-    }),
-  })
-    .then((response) => response.json())
-    .then((data) => ({
+      body: JSON.stringify({
+        Data: {
+          Email: email,
+          EmailPerm: "1",
+        },
+      }),
+    })
+
+    if (!response.ok) {
+      throw new Error("Error: " + response.status)
+    }
+
+    const data = await response.json()
+
+    return {
       statusCode: 200,
       body: JSON.stringify({ Msg: data }),
-    }))
-    .catch((error) => ({
-      statusCode: 422,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
       body: String(error),
-    }))
+    }
+  }
 }
